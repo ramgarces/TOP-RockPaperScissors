00c7f45d0846fa0c2052b9cc512a445526dcbfcc
Ramiro Garces <ramgarces24@gmail.com>
2023-08-10 16:44:21 -0500

    Add function to add points to scoreboard

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ function getPlayerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice == 'rock' && computerChoice == 'paper') {
-        return "You lose. Paper beats Rock.";
+        return "Computer wins. Paper beats Rock.";
     } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
         return "You win! Rock beats Scissors.";
     } else if (playerChoice == 'paper' && computerChoice == "scissors") {
-        return "You lose. Scissors beats Paper.";
+        return "Computer wins. Scissors beats Paper.";
     } else if (playerChoice == 'paper' && computerChoice == 'rock') {
         return "You win! Paper beats Rock.";
     } else if (playerChoice == "scissors" && computerChoice == "rock") {
-        return "You lose. Rock beats Scissors";
+        return "Computer wins. Rock beats Scissors";
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
         return "You win! Scissors beats Paper";
     } else {
@@ -32,22 +32,35 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice));
+function addPoints(playerScore, computerScore, playRound) {
+    let roundResult = playRound;
+    if (playRound.slice(0, 3) == "You") {
+        playerScore++;
+    } else if (playRound.slice(0, 3) == "Com") {
+        computerScore++;
     }
 }
 
-game();
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (i = 0; i < 5; i++) {
+//         let playerChoice = getPlayerChoice();
+//         let computerChoice = getComputerChoice();
+//         console.log(playRound(playerChoice, computerChoice));
+//     }
+// }
+
+// game();
 
 
-// let playerChoice = getPlayerChoice()
-// let computerChoice = getComputerChoice()
 
+let playerChoice = getPlayerChoice()
+let computerChoice = getComputerChoice()
+
+let playerScore = 0;
+let computerScore = 0;
+console.log(addPoints(playerScore, computerScore, playRound(playerChoice, computerChoice)))
 // console.log("Player choice: " + playerChoice)
 // console.log("Computer choice: " + computerChoice)
 // console.log(playRound(playerChoice, computerChoice))

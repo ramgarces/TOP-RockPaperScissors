bf317a75e73e7223f909025cea8695f6af3384e6
Ramiro Garces <ramgarces24@gmail.com>
2023-08-16 14:43:27 -0500

    Add event listeners to player selection buttons

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ function getComputerChoice() {
     }
 }
 
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+
 function getPlayerChoice() {
-    let playerChoice = 
-        prompt('Choose "Rock", "Paper", or "Scissors":').toLowerCase();
-    return playerChoice;
+    
+    rockBtn.addEventListener("click", function() {
+        console.log("rock")
+    });
+    paperBtn.addEventListener("click", function() {
+        console.log("paper");
+    });
+    scissorsBtn.addEventListener("click", function() {
+        console.log("scissors")
+    });
 }
 
 function playRound(playerChoice, computerChoice) {
@@ -33,30 +44,29 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice).slice(0,3);
-        if (result == "You") {
-            playerScore++;
-        } else if (result == "Com") {
-            computerScore++;
-        }
-        console.log("Player Choice :" + playerChoice);
-        console.log("Computer Choice: " + computerChoice);
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
-    }
-    if (playerScore > computerScore) {
-        console.log("You beat the computer!")
-    } else if (playerScore < computerScore) {
-        console.log("The computer beat you...")
-    } else {
-        console.log("It's a tie in the end.")
-    }
-}
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let playerChoice = getPlayerChoice();
+//     let computerChoice = getComputerChoice();
+//     let result = playRound(playerChoice, computerChoice).slice(0,3);
+//     if (result == "You") {
+//         playerScore++;
+//     } else if (result == "Com") {
+//         computerScore++;
+//     }
+//     console.log("Player Choice :" + playerChoice);
+//     console.log("Computer Choice: " + computerChoice);
+//     console.log("Player Score: " + playerScore);
+//     console.log("Computer Score: " + computerScore);
+//     if (playerScore > computerScore) {
+//         console.log("You beat the computer!")
+//     } else if (playerScore < computerScore) {
+//         console.log("The computer beat you...")
+//     } else {
+//         console.log("It's a tie in the end.")
+//     }
+// }
 
-game();
+// game();
+getPlayerChoice();
